831fdf26174b255fa5ae2ea42067bbb97dbf465c
Pete Lewis <pete.lewis@sparkfun.com>
2016-07-08 16:26:09 -0600

    updating to work with new shield hardware
    updated GPIO assignment for all LEDs.
    removed old LED assignments and related function calls
    created all_leds_off() funtion for use during programming cycle
    updated "main" while loop at the end to also watch the capsense net, and
    engage programming. Tweeked the counter amounts for snappy response.
    
    Added LED control to end of parse_results() function, to indicate
    statuses via LEDs. Kept debug printing for use with python terminal
    open.

--- test.py
+++ test.py
@@ -10,98 +10,90 @@
 # This contains all the calls to avrdude, including firmware, fuses and other flags
 # Indicate status on a few LEDs - ideally we can show success at fuse bits, program, and lock bits.
 
-
-
 import os, sys
 import shutil
 import serial
 import time
 import RPi.GPIO as GPIO
 
-FAILLED = 11
-PASSLED = 13
-STATLED = 7
-DETCLED = 15
+STAT = 7
+FUSE_P = 15
+FUSE_F = 13
+FLASH_P = 31
+FLASH_F = 11
+LOCK_P = 32
+LOCK_F = 22
 
+CAPSENSE = 33
 SHUTDOWN = 29
 
-BOOTLOADERPING = 31
-BARRELNOW = 32
-IO = 33
-RESETBUTTONS = 36
-
-BARRELNOW_LED_ON = False
-RESET_LED_ON = False
-STATLED_ON = False
-
-
-msg = 'NERD'
-runonce = 0
-trigger = 0
-resettimes1 = "0"
-resettimes2 = "0"
-resettimes3 = "0"
-heard0 = False
-heard1 = False
-testresult = False
-bootping = False
+STATLED_ON = False # Used for toggling stat led on and off during main loop
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(DETCLED, GPIO.OUT)
-GPIO.setup(STATLED, GPIO.OUT)
-GPIO.setup(FAILLED, GPIO.OUT)
-GPIO.setup(PASSLED, GPIO.OUT)
+
+GPIO.setup(STAT, GPIO.OUT)
+GPIO.setup(FUSE_P, GPIO.OUT)
+GPIO.setup(FUSE_F, GPIO.OUT)
+GPIO.setup(FLASH_P, GPIO.OUT)
+GPIO.setup(FLASH_F, GPIO.OUT)
+GPIO.setup(LOCK_P, GPIO.OUT)
+GPIO.setup(LOCK_F, GPIO.OUT)
 
 #GPIO.setup(SHUTDOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP) # with internal pullup enabled
 GPIO.setup(SHUTDOWN, GPIO.IN) # NO INTERNAL PULLUP
+GPIO.setup(CAPSENSE, GPIO.IN)
 
-GPIO.setup(BOOTLOADERPING, GPIO.OUT)
-GPIO.setup(BARRELNOW, GPIO.OUT)
-GPIO.setup(IO, GPIO.OUT)
-GPIO.setup(RESETBUTTONS, GPIO.OUT)
+GPIO.output(STAT, GPIO.LOW)
+GPIO.output(FUSE_P, GPIO.LOW)
+GPIO.output(FUSE_F, GPIO.LOW)
+GPIO.output(FLASH_P, GPIO.LOW)
+GPIO.output(FLASH_F, GPIO.LOW)
+GPIO.output(LOCK_P, GPIO.LOW)
+GPIO.output(LOCK_F, GPIO.LOW)
 
-GPIO.output(BOOTLOADERPING, GPIO.LOW)
-GPIO.output(BARRELNOW, GPIO.LOW)
-GPIO.output(IO, GPIO.LOW)
-GPIO.output(RESETBUTTONS, GPIO.LOW)
-
-
-GPIO.output(DETCLED, GPIO.LOW)
-GPIO.output(STATLED, GPIO.LOW)
-GPIO.output(FAILLED, GPIO.LOW)
-GPIO.output(PASSLED, GPIO.LOW)
-
-global firmware_path_media
-global new_hex
+firmware_path_media = ' '
 new_hex = False
-global copy_flag
 copy_flag = False
 
+def all_leds_on():
+        GPIO.output(STAT, GPIO.HIGH)
+        GPIO.output(FUSE_P, GPIO.HIGH)
+        GPIO.output(FUSE_F, GPIO.HIGH)
+        GPIO.output(FLASH_P, GPIO.HIGH)
+        GPIO.output(FLASH_F, GPIO.HIGH)
+        GPIO.output(LOCK_P, GPIO.HIGH)
+        GPIO.output(LOCK_F, GPIO.HIGH)
 
+def all_leds_off():
+        GPIO.output(STAT, GPIO.LOW)
+        GPIO.output(FUSE_P, GPIO.LOW)
+        GPIO.output(FUSE_F, GPIO.LOW)
+        GPIO.output(FLASH_P, GPIO.LOW)
+        GPIO.output(FLASH_F, GPIO.LOW)
+        GPIO.output(LOCK_P, GPIO.LOW)
+        GPIO.output(LOCK_F, GPIO.LOW)
+        
 def update_firmware():
         global new_hex
         new_hex = False
+        global firmware_path_media
+        global copy_flag
         for root, dirs, files in os.walk('/media'):
                 for name in files:
                         #print (os.path.join(root, name))
                         tempstring = (os.path.join(root, name))
                         if 'hex' in tempstring:
                                 #print 'new hex file found!!'
-                                global new_hex
                                 new_hex = True
                                 #print tempstring
-                                global firmware_path_media
                                 firmware_path_media = tempstring
         if(new_hex == False):
                 #print 'no new hex'
-                global copy_flag
                 copy_flag = False
         elif((new_hex == True) and (copy_flag == False)):
                 print 'new hex found'
-                global firmware_path_media
                 print firmware_path_media
-                global copy_flag
                 copy_flag = True
                 print 'copying hex file to local folder /home/pi'
                 shutil.copy(firmware_path_media, '/home/pi')
@@ -126,14 +118,23 @@ def program():
         print "...programming done."
 
 def parse_results():
+        #variables to store success/failure of each programming step
+        hfuse = False
+        lfuse = False
+        efuse = False
+        flash = False
+        lock = False
         f = open('fuse_results.txt', 'r')
         for line in f:
                 if 'avrdude: 1 bytes of hfuse verified' in line:
                         print line
+                        hfuse = True
                 elif 'avrdude: 1 bytes of lfuse verified' in line:
                         print line
+                        lfuse = True
                 elif 'avrdude: 1 bytes of efuse verified' in line:
                         print line
+                        efuse = True
                 elif 'avrdude: AVR device not responding' in line:
                         print line
         f.close()
@@ -141,32 +142,35 @@ def parse_results():
         f = open('flash_results.txt', 'r')
         for line in f:
                 if 'avrdude: 32768 bytes of flash verified' in line:
-                        print line                  
+                        print line
+                        flash = True
                 elif 'avrdude: 1 bytes of lock verified' in line:
                         print line
                 elif 'avrdude: AVR device not responding' in line:
                         print line
         f.close()        
-
+        ## display results on all 6 stat LEDs
+        if((hfuse == True) and (lfuse == True) and (efuse == True)):
+                GPIO.output(FUSE_P, GPIO.HIGH)
+        else:
+                GPIO.output(FUSE_F, GPIO.HIGH)
+        if(flash == True):
+                GPIO.output(FLASH_P, GPIO.HIGH)
+        else:
+                GPIO.output(FLASH_F, GPIO.HIGH)
+        if(lock == True):
+                GPIO.output(LOCK_P, GPIO.HIGH)
+        else:
+                GPIO.output(LOCK_F, GPIO.HIGH)                  
+        
 def toggle_stat_led():
         global STATLED_ON
         if (STATLED_ON):
-                GPIO.output(STATLED, GPIO.LOW)
+                GPIO.output(STAT, GPIO.LOW)
                 STATLED_ON = False
         else:
-                GPIO.output(STATLED, GPIO.HIGH)
+                GPIO.output(STAT, GPIO.HIGH)
                 STATLED_ON = True
-
-def all_leds_on(): # used to indicate proper shutdown
-        GPIO.output(BOOTLOADERPING, GPIO.HIGH)
-        GPIO.output(BARRELNOW, GPIO.HIGH)
-        GPIO.output(IO, GPIO.HIGH)
-        GPIO.output(RESETBUTTONS, GPIO.HIGH)
-        GPIO.output(DETCLED, GPIO.HIGH)
-        GPIO.output(STATLED, GPIO.HIGH)
-        GPIO.output(FAILLED, GPIO.HIGH)
-        GPIO.output(PASSLED, GPIO.HIGH)
-
 while True:
         toggle_stat_led() 
         time.sleep(.5)
@@ -181,9 +185,19 @@ while True:
                         counter += 1
                         print counter
                         time.sleep(.5)
-                if counter < 4:
-                        program()
-                        parse_results()
-                if counter == 4:
-                        all_leds_on()
-                        shut_down()
+                        if counter > 4:
+                                all_leds_on()
+                                shut_down()
+
+        if GPIO.input(CAPSENSE) == True:
+                counter = 0
+                while GPIO.input(CAPSENSE) == True:
+                        counter += 1
+                        print counter
+                        time.sleep(.1)
+                        if counter > 2:
+                                program()
+                                parse_results()
+                                time.sleep(3) # led leds stay on for 3 secs
+                                all_leds_off()
+                                break
