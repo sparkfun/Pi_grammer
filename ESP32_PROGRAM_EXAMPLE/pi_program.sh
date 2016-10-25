a8be5608884121ad016e6b2136e61bc2354d75d5
Pete Lewis <pete.lewis@sparkfun.com>
2016-10-25 16:22:04 -0600

    speeding up esp32 programming to 921600

--- ESP32_PROGRAM_EXAMPLE/pi_program.sh
+++ ESP32_PROGRAM_EXAMPLE/pi_program.sh
@@ -6,4 +6,4 @@ echo "Programming beginning..."
 firmware=$(find /home/pi/*.bin)
 $firmware .= "/home/pi/$firmware"
 
-sudo python /home/pi/espressif/esp32/tools/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 write_flash -z --flash_freq 80m --flash_mode dio 0x1000 /home/pi/espressif/esp32/tools/sdk/bin/bootloader.bin 0x4000 /home/pi/espressif/esp32/tools/sdk/bin/partitions_singleapp.bin 0x10000 $firmware 1>/home/pi/flash_results.txt
+sudo python /home/pi/espressif/esp32/tools/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 write_flash -z --flash_freq 80m --flash_mode dio 0x1000 /home/pi/espressif/esp32/tools/sdk/bin/bootloader.bin 0x4000 /home/pi/espressif/esp32/tools/sdk/bin/partitions_singleapp.bin 0x10000 $firmware 1>/home/pi/flash_results.txt
