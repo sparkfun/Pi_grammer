cede6dac572d1cdda590db1d86694be0a807afdb
Elias Santistevan <elias.santistevan@sparkfun.local>
2017-12-21 08:39:21 -0700

    pi_serial_upload default settings and variables
    I added a device and baud rate to the top of this script similar to the
    pi_upload shell script. I changed the programmer command (-c) to default
    to arduino instead of wiring, the default device to atmega328p, and the
    baud to 115200. I also put notes in the script to pick a slower baud
    rate for slower clock speesd ( <= 8mHz)

--- SERIAL_UPLOAD/pi_serial_upload.sh
+++ SERIAL_UPLOAD/pi_serial_upload.sh
@@ -2,10 +2,15 @@
 
 echo "Serial Upload beginning..."
 
+# Device and Baudrate. The default baudrate of 115200 will work with
+# all chips above an 8mHz clock speed. 57600 is recommended for slower chips.
+DEVICE=atmega328p
+BAUD=115200
+
 # get firmware file name
 firmware=$(find /home/pi/SERIAL_UPLOAD/*.hex)
 $firmware .= "home/pi/SERIAL_UPLOAD/$firmware"
 echo "firmware: "
 echo $firmware
 
-sudo /usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -v -patmega2560 -cwiring -P/dev/ttyUSB0 -b115200 -D -Uflash:w:$firmware:i 2>/home/pi/SERIAL_UPLOAD/serial_upload_results.txt
+sudo /usr/share/arduino/hardware/tools/avrdude -C/usr/share/arduino/hardware/tools/avrdude.conf -v -p$DEVICE -carduino -P/dev/ttyUSB0 -b$BAUD -D -Uflash:w:$firmware:i 2>/home/pi/SERIAL_UPLOAD/serial_upload_results.txt
